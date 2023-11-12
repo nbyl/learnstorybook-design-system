2089482fa864f855d154823c70cae2cf492c8e1c
Nicolas Byl <nico@nicolas-byl.eu>
2023-11-12 19:23:32 +0100

    feat: backup

---
+++ .storybook/preview.jsx
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  decorators: [
+    (Story) => (
+      <>
+        <Global styles={GlobalStyle} />
+        <Story />
+      </>
+    ),
+  ],
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
